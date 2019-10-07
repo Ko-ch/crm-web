5d55eccbabaa9ec7b9796d18285da7f84b00b4ee
Koichiro <koichiro_takagi@outlook.jp>
2019-10-07 21:04:52 +0900

    新規顧客の追加用のhtmlを作成したよ

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def index():
     return render_template("index.html", customers=customers)
 
 
+@app.route("/add")
+def add():
+    customer = addcustomer()
+
+
 if __name__ == "__main__":
     app.run(debug=True)
